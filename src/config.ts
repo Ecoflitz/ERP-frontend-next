c442bdc9e00f5e6f7711744c3be87a3a31f61ce3
Ecoflitz <admin@ecoflitz.co.uk>
2025-01-21 01:09:31 +0500

    updated config and menu

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
-export const hostApi =
-  process.env.NODE_ENV === 'development' ? 'http://localhost' : '';
+export const hostApi = 'https://erp.ecoflitz.com';
+// process.env.NODE_ENV === 'development' ? 'http://localhost' : 'https://crm.ecoflitz.com';
 export const portApi = process.env.NODE_ENV === 'development' ? 8080 : '';
-export const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}/api`;
+// export const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}/api`;
+
+export const baseURLApi = `${hostApi}/api`;
 
 export const localStorageDarkModeKey = 'darkMode';
 
@@ -9,7 +11,7 @@ export const localStorageStyleKey = 'style';
 
 export const containerMaxW = 'xl:max-w-full xl:mx-auto 2xl:mx-20';
 
-export const appTitle = 'created by ecoflitz generator!';
+export const appTitle = 'created by ecoflitz';
 
 export const getPageTitle = (currentPageTitle: string) =>
   `${currentPageTitle} — ${appTitle}`;
